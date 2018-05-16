268b3b19587f3005c59958f8a753d0128b8360cc
Ming Lin <flaaud159@gmail.com>
2018-05-16 22:34:49 +0800

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -251,5 +251,15 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   //
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
-  return {};
+  //return all the steering angle & accel and state variable.
+//first two variable are steering angle and acceleration value.
+vector<double> solved_value;
+solved_value.push_back(solution.x[delta_start]);
+solve_value.push_back(solution.x[a_start]);
+// after delta and accel, vehicle position x, y are also added.
+for (int i = 0; i < N; i++){
+	solved_value.push_back(solution.x[x_start + i]);
+	solved_value.push_back(solution.y[y_start + i]);
+}
+  return solved_value;
 }
